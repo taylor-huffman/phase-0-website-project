542bd4673b5cc127ee6572c11b041b38172c7216
Taylor Huffman <taylor@crucialartists.com>
2021-11-30 11:16:09 -0600

    Started Nav and Hero Section

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by Javascript";
-document.querySelector("body").appendChild(h2);
+// const h2 = document.createElement("h2");
+// h2.textContent = "This content added by Javascript";
+// document.querySelector("body").appendChild(h2);
