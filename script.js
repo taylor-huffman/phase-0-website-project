6c1dffc5b8ef1c5700647d6cc0bfb2fb2351664c
Taylor Huffman <taylor@crucialartists.com>
2021-12-01 18:55:20 -0600

    Refined mobile menu slide-in

--- script.js
+++ script.js
@@ -5,17 +5,19 @@
 const hamburger_menu = document.getElementById('mobile-menu-toggle');
 const mobile_menu_container = document.getElementById('mobile-menu-container');
 const mobile_menu_close = document.getElementById('mobile-menu-close');
+const mobile_menu = document.getElementById('mobile-menu');
 const body = document.querySelector('body');
 
 hamburger_menu.addEventListener('click', function() {
-    // alert('I was clicked!');
-    mobile_menu_container.style.display = "block";
-    mobile_menu_container.style.backgroundColor = "rgba(0 0 0 / 75%)";
+    mobile_menu_container.style.visibility = "visible";
+    mobile_menu_container.style.backgroundColor = "rgba(20 31 53 / 88%)";
+    mobile_menu.classList.toggle('mobile-menu-open');
     body.style.overflow = "hidden";
 });
 
 mobile_menu_container.addEventListener('click', function () {
-    mobile_menu_container.style.display = "none";
-    mobile_menu_container.style.backgroundColor = "rgba(0 0 0 / 0%)"
+    mobile_menu_container.style.visibility = "hidden";
+    mobile_menu_container.style.backgroundColor = "rgba(20 31 53 / 0%)";
+    mobile_menu.classList.toggle('mobile-menu-open');
     body.style.overflow = "auto";
 });
