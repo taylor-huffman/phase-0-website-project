1821d1836b5f9b1ca40ddc1b7c2be32f28b00926
Taylor Huffman <taylor@crucialartists.com>
2021-12-02 09:53:57 -0600

    Added back to top button

--- script.js
+++ script.js
@@ -2,22 +2,35 @@
 // h2.textContent = "This content added by Javascript";
 // document.querySelector("body").appendChild(h2);
 
-const hamburger_menu = document.getElementById('mobile-menu-toggle');
-const mobile_menu_container = document.getElementById('mobile-menu-container');
-const mobile_menu_close = document.getElementById('mobile-menu-close');
-const mobile_menu = document.getElementById('mobile-menu');
 const body = document.querySelector('body');
+const hamburgerMenu = document.getElementById('mobile-menu-toggle');
+const mobileMenuContainer = document.getElementById('mobile-menu-container');
+const mobileMenuClose = document.getElementById('mobile-menu-close');
+const mobileMenu = document.getElementById('mobile-menu');
+const backToTopButton = document.getElementById('back-to-top-button');
 
-hamburger_menu.addEventListener('click', function() {
-    mobile_menu_container.style.visibility = "visible";
-    mobile_menu_container.style.backgroundColor = "rgba(20 31 53 / 88%)";
-    mobile_menu.classList.toggle('mobile-menu-open');
+hamburgerMenu.addEventListener('click', function() {
+    mobileMenuContainer.style.visibility = "visible";
+    mobileMenuContainer.style.backgroundColor = "rgba(20 31 53 / 88%)";
+    mobileMenu.classList.toggle('mobile-menu-open');
     body.style.overflow = "hidden";
 });
 
-mobile_menu_container.addEventListener('click', function () {
-    mobile_menu_container.style.visibility = "hidden";
-    mobile_menu_container.style.backgroundColor = "rgba(20 31 53 / 0%)";
-    mobile_menu.classList.toggle('mobile-menu-open');
+mobileMenuContainer.addEventListener('click', function () {
+    mobileMenuContainer.style.visibility = "hidden";
+    mobileMenuContainer.style.backgroundColor = "rgba(20 31 53 / 0%)";
+    mobileMenu.classList.toggle('mobile-menu-open');
     body.style.overflow = "auto";
 });
+
+window.onscroll = function() {backToTop()};
+
+function backToTop() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        // backToTopButton.style.visibility = "visible";
+        backToTopButton.classList.add('back-to-top-button-show');
+    } else {
+        // backToTopButton.style.visibility = "hidden";
+        backToTopButton.classList.remove('back-to-top-button-show');
+    }
+  }
